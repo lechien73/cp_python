2bf0bc354fe56f42314810f28a66b483a520fd12
Matt Rudge <matt.rudge@codeinstitute.net>
2024-09-24 10:17:58 +0100

    Update 03_fibs.py

--- 03_fibs.py
+++ 03_fibs.py
@@ -1,9 +1,9 @@
 def fibs(n):
-	result = [0, 1]
-	for i in range(n-2):
-		result.append(result[-2] + result[-1])
-	return result
+    result = [0, 1]
+    for i in range(n-2):
+        result.append(result[-2] + result[-1])
+    return result
 
 if __name__ == '__main__':
-	n = int(input('How many Fibonacci numbers do you want? '))
-	print(fibs(n))
+    n = int(input('How many Fibonacci numbers do you want? '))
+    print(fibs(n))
